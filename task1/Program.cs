68c3166dfca8a10ae5d72db90c441a49bad083ea
Kristina <elkina.kv@gmail.com>
2024-06-18 21:40:58 +0300

    add DZ

---
+++ task1/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 1: 
+// Задайте значения M и N. 
+// Напишите программу, которая выведет все натуральные числа
+// в промежутке от M до N. 
+// Использовать рекурсию, не использовать циклы.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 5, 6, 7, 8"
+
+void Main()
+{
+    int M = ReadInt("Введите значение M: ");
+    int N = ReadInt("Введите значение N: ");
+    Console.Write("Значения от M до N: ");
+    PrintNumbersMN(M, N);
+    PrintNumbersMN2(M, N);
+}
+
+// если M < N, вывод от M до N
+void PrintNumbersMN(int M, int N)
+{
+    if (N < M) return;
+    PrintNumbersMN(M, N - 1);
+    Console.Write(N + ", ");
+}
+
+// если M > N, вывод от M до N
+void PrintNumbersMN2(int M, int N)
+{
+    if (N > M) return;
+    PrintNumbersMN2(M, N + 1);
+    Console.Write(N + ", ");
+}
+
+int ReadInt(string msg)
+{
+    Console.Write(msg);
+    return Math.Abs(Convert.ToInt32(Console.ReadLine()));
+}
+
+Main();
---
+++ task3/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 3: 
+// Задайте произвольный массив. 
+// Выведете его элементы, начиная с конца.
+// Использовать рекурсию, не использовать циклы.
+// [1, 2, 5, 0, 10, 34] => 34 10 0 5 2 1
+
+void Main()
+{
+    int arraySize = ReadInt("Введите размер массива: ");
+    int[] array = new int[arraySize];
+
+    GenerateArray(array);
+
+    PrintArray(array);
+
+    Console.WriteLine();
+    PrintReverseArray(array);
+}
+
+void PrintReverseArray(int[] array, int i = 0)
+{
+    if (i >= array.Length) return;
+    PrintReverseArray(array, i + 1);
+    Console.Write(array[i] + " ");
+}
+
+void PrintArray(int[] array, int i = 0)
+{
+    if (i >= array.Length) return;
+    Console.Write(array[i] + " ");
+    PrintArray(array, i + 1);
+}
+
+void GenerateArray(int[] array, int i = 0)
+{
+    Random rnd = new Random();
+    if (i >= array.Length) return;
+    array[i] = rnd.Next(1, 100);
+    GenerateArray(array, i + 1);
+}
+
+int ReadInt(string msg)
+{
+    Console.Write(msg);
+    return Math.Abs(Convert.ToInt32(Console.ReadLine()));
+}
+
+Main();
